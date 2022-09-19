4a1b7fec28a4b1df445bb010f256da46cea3788b
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-09-19 17:15:49 +0530

    added about section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { SocialIcon } from "react-social-icons"
 import { motion } from "framer-motion"
-const Header = () => {
+type Props = {}
+
+const Header = (props: Props) => {
     return (
-        <header className='sticky p-5 top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+        <header className='sticky first-letter:top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
             <motion.div
                 initial={{ x: -500, opacity: 0, scale: 0.5 }}
                 animate={{ x: 0, opacity: 1, scale: 1 }}
                 transition={{ duration: 1.5 }}
-
-                className='flex flex-row  items-center justify-center'>
+                className='flex flex-row items-center blur-md'>
                 {/* Social Icons */}
                 <SocialIcon fgColor="gray" bgColor="transparent" url="https://www.youtube.com/channel/UCQdcHzF8tGL_Ska-Hj5x0RQ" network="youtube" className='hover:scale-125 duration-300 hover:text-white' />
                 <SocialIcon fgColor="gray" bgColor="transparent" url="https://www.instagram.com/prajwalladkat0805/" network="instagram" className='hover:scale-125 duration-300 hover:text-white' />
@@ -20,12 +21,12 @@ const Header = () => {
                 initial={{ x: 500, opacity: 0, scale: 0.5 }}
                 animate={{ x: 0, opacity: 1, scale: 1 }}
                 transition={{ duration: 1.5 }}
-                className=' flex flex-row items-center'>
+                className='flex flex-row items-center text-gray-300 cursor-pointer'>
                 <SocialIcon network='email' className='cursor-pointer hover:scale-125 duration-300' fgColor='gray' bgColor='transparent ' />
-                <p className='hidden md:inline-flex text-sm text-gray-400 uppercase'>Get In Touch</p>
+                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
             </motion.div>
         </header>
     )
 }
 
-export default Header
+export default Header   
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,44 +1,41 @@
-import Image from 'next/image';
 import React from 'react'
-import { motion } from 'framer-motion';
-import { Cursor, useTypewriter } from "react-simple-typewriter"
-import image from "../../Assets/profile.jpg";
+import Image from "next/image"
+import image from "../Assets/profile.jpg"
+import { motion } from 'framer-motion'
 import Link from "next/link"
-import BackgroundCircles from '../BackgroundCircles/BackgroundCircles';
+import { Cursor, useTypewriter } from "react-simple-typewriter"
+import BackgroundCircles from './BackgroundCircles'
+
 type Props = {}
 
-type StaticImageData = { image: string; height: number; width: number; blurDataURL?: string; }
-
 const Hero = (props: Props) => {
-
     const [text, count] = useTypewriter({
         words: ["Hi, Prajwal Ladkat here ✌️", "Guy who-loves-coffee.tsx", "<ButLovesToCodeMore />"],
         loop: true,
         delaySpeed: 2000,
-    });
+    })
     return (
         <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
-            <BackgroundCircles />
-            <Image src={image} className="relative rounded-full h-52 w-22 mx-auto" />
-
+            <BackgroundCircles/>
+            <Image src={image} className="relative rounded-full h-52 w-22 mx-auto object-cover" alt="profile" />
             <div className='z-20'>
-                <h2 className='text-xs md:text-sm font-bold uppercase text-gray-500 pb-2 tracking-[10px] md:tracking-[15px]'>Software Engineer</h2>
-                <h1 className='text-xl md:text-3xl lg:text-6xl font-semibold px-10'>
-                    <span className='mr-3'>{text}</span>
-                    <Cursor cursorColor='#F7AB0A' />
-                </h1>
-                <motion.div
+                <h2 className='text-xs uppercase text-gray-500 pb-2 tracking-[10px] md:tracking-[15px]'>Software Engineer</h2>
+            <h1 className='text-2xl md:text-4xl lg:text-6xl font-semibold px-10'>
+                <span className=' '>{text}</span>
+                <Cursor cursorColor='#f7ab0a' />
+            </h1>
+            <motion.div
                     initial={{ opacity: 0, y: -100 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 2.5 }}
-                    className='pt-5'>
+                    className='p-8 justify-center flex'>
                     <Link href="#about"><button className="heroButton">About</button></Link>
                     <Link href="#skills"><button className="heroButton">Skills</button></Link>
                     <Link href="#experience"><button className="heroButton">Experience</button></Link>
                     <Link href="#projects"><button className="heroButton">Projects</button></Link>
                 </motion.div>
             </div>
-        </div >
+        </div>
     )
 }
 
