cf46a33ceb62cc3af5528707bda5eb350c35ff42
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-09-23 16:02:48 +0530

    fixed

--- sanity.ts
+++ sanity.ts
@@ -1,9 +1,9 @@
-import {createClient} from "next-sanity"
+import { createClient } from "next-sanity"
 import imageUrlBuilder from "@sanity/image-url";
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    projectId: "h31zqnzw",
     apiVersion: "2021-03-25",
     useCdn: process.env.NODE_ENV === "production",
 }
@@ -13,7 +13,7 @@ export const sanityClient = createClient(config)
 
 const builder = imageUrlBuilder(sanityClient)
 
-export const urlFor = (source:any) => {
+export const urlFor = (source: any) => {
     return builder.image(source)
 }
 
