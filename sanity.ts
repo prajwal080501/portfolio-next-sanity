eef8c186c0dd88f76a030058256f57300279660a
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-11-30 13:08:14 +0530

    added env

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: "h31zqnzw",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: "2021-03-25",
     useCdn: process.env.NODE_ENV === "production",
 }
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,6 +1,6 @@
 import { Project } from './../typings.d';
 export const fetchProjects = async () => {
-    const res = await fetch('http://localhost:3000/api/getProjects');
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
 
 
     const data = await res.json();
