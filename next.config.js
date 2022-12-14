df20ee6f25fecb1fc380b4c5ab92c0cfb4dd32c9
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-12-14 18:03:03 +0530

    added pi

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL:"http://localhost:3000",
+    NEXT_PUBLIC_SANITY_DATASET: "production",
+    NEXT_PUBLIC_SANITY_PROJECT_ID:"h31zqnzw",
+  },
 }
 
 module.exports = nextConfig
