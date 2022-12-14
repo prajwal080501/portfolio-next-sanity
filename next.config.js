df4115753d76aaf42950e7cc8dcfd44dce85e661
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-12-14 18:21:08 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_BASE_URL:"http://localhost:3000",
+    NEXT_PUBLIC_BASE_URL:"https://portfolio-next-sanity-green.vercel.app/",
     NEXT_PUBLIC_SANITY_DATASET: "production",
     NEXT_PUBLIC_SANITY_PROJECT_ID:"h31zqnzw",
   },
