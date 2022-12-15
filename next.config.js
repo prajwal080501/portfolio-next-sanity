be2e3838800bd66135454fed3d9aef99cd996d0a
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-12-15 18:23:22 +0530

    updated url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_BASE_URL:"https://portfolio-next-sanity-green.vercel.app/",
+    NEXT_PUBLIC_BASE_URL:"https://prajwalladkatportfolio.vercel.app",
     NEXT_PUBLIC_SANITY_DATASET: "production",
     NEXT_PUBLIC_SANITY_PROJECT_ID:"h31zqnzw",
   },
